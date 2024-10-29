e6af7e05d8bd8894242c7a7c9f97f3a844d7ff49
Facundo Lemo <facundolemo@gmail.com>
2024-10-29 18:29:25 -0300

    removed dark mode and first version of profile

--- src/app/usuario/[id]/page.jsx
+++ src/app/usuario/[id]/page.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client"
+import FetchData from '@/components/FetchData'
+import { AuthContext } from '@/contexts/AuthContext';
+import React, { useState, useEffect, useContext, use } from 'react'
 
-function page() {
+function Page(context) {
+  const params = use(context.params),
+    { id } = params,
+    { token, authStatus } = useContext(AuthContext),
+    [user, setUser] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (token) {
+        try {
+          const userData = await FetchData(`users/${id}/`, token);
+          setUser(userData);
+        } catch (error) {
+          console.error('Error loading user data', error);
+        }
+      }
+    };
+
+    if (authStatus === 'authenticated') {
+      fetchData();
+    } else if (authStatus === 'unauthenticated') {
+      router.push('/login');
+    }
+
+  }, [id, authStatus, token]);
+
+  if (!user) {
+    return <p className='text-3xl font-semibold animate-pulse text-center p-20'>Loading...</p>;
+  }
+
   return (
-    <div>pepe</div>
+    <div className='text-center pt-8'>
+
+      <div className='text-xl'>Bienvenido al perfil de </div>
+      <div className='text-7xl mb-8 italic'> {user.name}</div>
+
+      <hr className='mx-28 '/>
+
+      {/* Agregar acá la biblioteca del usuario */}
+    </div>
   )
 }
 
-export default page
+export default Page;
